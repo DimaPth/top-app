44511fed374a4af3b822bded757f784c1a59560a
Dima <mr.petuhdima@mail.ru>
2021-07-17 13:47:33 +0300

    completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Button, Htag, P, Tag } from "../components";
+import React, { useState } from "react";
+import { Button, Htag, P, Rating, Tag } from "../components";
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
+
 	return (
 		<>
 			<Htag tag="h1">text</Htag>
@@ -19,6 +21,7 @@ export default function Home(): JSX.Element {
 			<Tag color="grey">Lorem, ipsum dolor.</Tag>
 			<Tag color="primary">Lorem, ipsum dolor.</Tag>
 			<Tag color="ghost">Lorem, ipsum dolor.</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
